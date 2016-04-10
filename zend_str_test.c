b67c2098df24242cb4cb2440109a2327f5448e34
yanruitao <luluyrt@163.com>
2016-04-10 12:19:51 +0800

    php引擎中字符串的例子

---
+++ zend_str_test.c
@@ -0,0 +1,33 @@
+// Example program
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+typedef struct _zend_string{
+	size_t len;
+	char val[1];
+} zend_string;
+
+static zend_string *zend_string_init(const char *, size_t);
+static zend_string *zend_string_alloc(size_t);
+
+static zend_string *zend_string_init(const char *str, size_t len)
+{
+	zend_string *ret = zend_string_alloc(len);
+
+	memcpy(ret->val, str, len);
+	ret->len = len;
+	return ret;
+}
+
+static zend_string *zend_string_alloc(size_t len)
+{
+	zend_string *ret = (zend_string*)malloc(sizeof(size_t) + len);
+	return ret;
+}
+
+int main()
+{
+  char const *src = "我是吴彦祖！";
+  zend_string *zs = zend_string_init(src, sizeof("我是吴彦祖！"));
+  printf("%s\n%d\n%d\n%d\n", zs->val, zs->len, sizeof(size_t), sizeof("我是吴彦祖！"));
+}
